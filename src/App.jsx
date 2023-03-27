98db85ec46caf75612e535f36948a3c9fe0cb314
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2023-03-27 19:02:48 +0300

    Temperature Converter(simple render props example)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Input(props) {
+  const [value, setValue] = useState("");
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Temp in °C"
+      />
+      {props.render(value)}
+    </>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <div className="App">
+      <h1>☃️ Temperature Converter 🌞</h1>
+      <Input
+        render={value => (
+          <>
+            <Kelvin value={value} />
+            <Fahrenheit value={value} />
+          </>
+        )}
+      />
+    </div>
+  );
+}
+
+function Kelvin({ value }) {
+  return <div className="temp">{parseInt(value || 0) + 273.15}K</div>;
+}
+
+function Fahrenheit({ value }) {
+  return <div className="temp">{(parseInt(value || 0) * 9) / 5 + 32}°F</div>;
+}
